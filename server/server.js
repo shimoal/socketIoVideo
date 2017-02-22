67e292e52b0d791b501099bea9801dd44d23783e
Alison Reed <alison.michelle.reed@gmail.com>
2017-02-22 11:50:35 -0800

    Create peer connection for video

--- server/server.js
+++ server/server.js
@@ -5,10 +5,10 @@ var io = require('socket.io')(http);
 
 var numUsers = 0;
 var roomNo = 1;
-var user1;
-var user2;
-
 var port = process.env.PORT || 3000;
+
+
+
 
 app.use(express.static(__dirname + '/../public'));
 
@@ -16,25 +16,23 @@ io.on('connection', function(socket) {
   console.log('A user connected!');
   numUsers++;
 
-  socket.join("room-" + roomNo);
 
-
-  socket.on('call', function(name) {
-    socket.broadcast.emit('receiveCall', name);
+  socket.on('sendDescription', function(data) {
+    socket.broadcast.emit('description', data);
   });
 
-  socket.on('answer', function(name) {
-    socket.broadcast.emit('receiveAnswer', name);
-  })
 
-  socket.on('sendCall', function(name) {
-    socket.emit('answerCall', name);
-  })
+  socket.on('sendCandidate', function(data){
+    socket.broadcast.emit('candidate', data);
+  });
+
+
+  
+
 
   io.sockets.in("room-"+roomNo).emit('newUser', numUsers);
 
   socket.on('disconnect', function() {
-    socket.leave("room-" + roomNo);
     console.log('A user disconnected');
     numUsers--;
   })
